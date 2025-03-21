1f40da677c3b651b747d1893ed9ca42625e9e79e
Calebe Rodrigues <158089367+calebeluz@users.noreply.github.com>
2025-03-21 14:28:18 -0300

    Update Chave.sh

--- Chave.sh
+++ Chave.sh
@@ -1,5 +1,5 @@
 git config --global user.name "Calebe Rodrigues Luz"
-git congig --global user.email"calebe.luz08@aluno.ifce.edu.br"
+git config --global user.email"calebe.luz08@aluno.ifce.edu.br"
 
 cp ~/.ssh/20231321000029 ~/ssh/id_ed25519
 cp ~/.ssh/20231321000029.pub ~pub/.ssh/id_ed25519.pub
